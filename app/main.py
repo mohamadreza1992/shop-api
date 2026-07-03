c5d9c4c5d42ba6fc4d6f9e72a18fddb91bf68d1b
mohamadreza1992 <khoshniatmohamadreza@gmail.com>
2026-07-03 12:20:11 +0330

    add create product API (POST /products)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from app.schemas import ProductCreate
+from app.store import Store
 
 app= FastAPI()
+store=Store()
 
 @app.get("/")
 def home():
     return{"message":"first routh "}
+
+@app.post("/products")
+def create_product(product: ProductCreate):
+    store.add_product(product)
+    return {"message": "Product added successfully"}
---
+++ app/store.py
@@ -0,0 +1,5 @@
+class Store:
+    def __init__(self):
+        self.products=[]
+    def add_products(self,product):
+        self.products.append(product)        
